d3c0d94fe2d20923df28a09a16c9e2f664a3a2cd
linxifanlove <lin851002@126.com>
2018-04-19 15:03:12 +0800

    x
    x

--- main.cpp
+++ main.cpp
@@ -5,11 +5,6 @@ using namespace std;
 void main()
 {
 	srand(time(NULL));
-
-		m_accessKeyId = ak;
-		m_accessSecret = aks + "&";
-		m_signName = specialUrlEncode(signName.c_str());
-		m_templateCode = templateCode;
 	string ak = "yourAccessKeyId";
 	string aks = "yourAccessKeySecret";
 	string signName = "yourSignName";
@@ -18,7 +13,7 @@ void main()
 	CAliyunSMS sms;
 	sms.init(ak, aks, signName, templateCode);
 	int shortCode = rand() % 9000 + 1000;//发送给手机的验证码
-	string phone = "13751762631";
+	string phone = "137xxxxxxxxx";
 	sms.sendSm(shortCode, phone);
 
 	getchar();
